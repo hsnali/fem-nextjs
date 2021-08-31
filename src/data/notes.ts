0acff10a09b4a8d0a77a496be00cd668381b2dbb
Hassan Ali <hassan.ali@empired.com>
2021-08-31 19:43:58 +1000

    underscore

--- src/data/notes.ts
+++ src/data/notes.ts
@@ -1,6 +1,6 @@
 const notes = new Array(15)
   .fill(1)
-  .map((item, index) => ({ id: Date.now() + index, title: `Note ${index}` }));
+  .map((_, index) => ({ id: Date.now() + index, title: `Note ${index}` }));
 
 // module.exports = notes;
 export default notes;
