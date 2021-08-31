a397c5b8ac63db88642fb9571d53f3835a4e526c
Hassan Ali <hassan.ali@empired.com>
2021-08-31 19:40:59 +1000

    seed some data

--- src/data/notes.ts
+++ src/data/notes.ts
@@ -1,4 +1,6 @@
-const notes = [{ title: "Foo", id: 1 }];
+const notes = new Array(15)
+  .fill(1)
+  .map((item, index) => ({ id: Date.now() + index, title: `Note ${index}` }));
 
 // module.exports = notes;
 export default notes;
